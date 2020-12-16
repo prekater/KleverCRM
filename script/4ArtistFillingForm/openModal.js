3b96967fd5b51570e3a8cb87a6546c02b1e94f2a
prekater <pepekatya@gmail.com>
2020-12-16 13:10:39 +0300

    Всплывающие подсказки шаг1 мобильная версия

---
+++ script/4ArtistFillingForm/openModal.js
@@ -0,0 +1,19 @@
+p = $('.popup__overlay')
+body = $('body')
+$('.question').click(function() {
+    p.css('display', 'block')
+    $(body).css('overflow', 'hidden')
+    popupText = $(this).attr('tooltip')
+    $('.popup__content').text(popupText)
+})
+p.click(function(event) {
+    e = event || window.event
+    if (e.target == this) {
+        $(p).css('display', 'none')
+        $(body).css('overflow', 'auto')
+    }
+})
+$('.popup__close').click(function() {
+    p.css('display', 'none')
+    $(body).css('overflow', 'auto')
+})
