2f3df657645b79b3ccd8f4709d00172bef444009
prekater <pepekatya@gmail.com>
2021-01-15 14:21:00 +0300

    отстутпы между part  в sidebar, успешная отправка

--- src/track-releases/scripts/index.js
+++ src/track-releases/scripts/index.js
@@ -1,3 +1,36 @@
+// плагин для  выпадающего меню 
 $(document).on("ready", function() {
     $('select').niceSelect();
 });
+
+// подсказки popup в мобилке
+
+$(function() {
+    if ($(window).width() <= 575.9) {
+        p = $('.popup__overlay')
+        popup = $(".popup")
+        body = $('body')
+        $('.field__question').on("click", function() {
+            p.css('display', 'block')
+            $(body).css('overflow', 'hidden')
+            popup.css('animation', 'translating 0.3s linear 1')
+            popupText = $(this).attr('tooltip')
+            $('.popup__content').text(popupText)
+
+        });
+
+        p.click(function(event) {
+            e = event || window.event
+            if (e.target === this) {
+                $(p).css('display', 'none')
+                $(body).css('overflow', 'auto')
+            }
+        })
+        $('.popup__close').on("click", function() {
+            p.css('display', 'none')
+            $(body).css('overflow', 'auto')
+        });
+    }
+});
+
+//переход с кнопки Отправить на экран Успешно создан
