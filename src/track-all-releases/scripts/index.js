7853e61a4d5dd9250e5d5e392c1197e50f720fca
prekater <pepekatya@gmail.com>
2021-01-22 22:21:16 +0300

    отступ лупы в поле поиска таблицы

--- src/track-all-releases/scripts/index.js
+++ src/track-all-releases/scripts/index.js
@@ -24,31 +24,17 @@ $(function() {
     }
 });
 
-//поле поиска в таблице десктоп
+//поле поиска в таблице
 let $searchTable = $("#search-table");
 let $searchTableInput = $(".search-table__input");
+let $searchTableBtnMob = $(".search-table__button-mobile");
+let $searchTableClear = $(".search-table__clear");
+let $title = $(".all-releases-header__title");
+
 
 $searchTable.on("focusin", function() {
     $searchTableInput.css("width", "100%");
 });
-
-$searchTable.on("focusout", function() {
-    if (!(($searchTableInput.val() === null) || ($searchTableInput.val() === "") || ($searchTableInput.val() === undefined))) {
-        $searchTableInput.css("width", "100%");
-    } else {
-        if ($(window).width() <= 991.9) {
-            $searchTableInput.css("width", "88px");
-        } else {
-            $searchTableInput.css("width", "244px");
-        };
-        $searchTableInput.css("background-color", "white");
-    }
-});
-
-//поле поиска в таблице мобильная версия
-let $searchTableBtnMob = $(".search-table__button-mobile");
-let $searchTableClear = $(".search-table__clear");
-let $title = $(".all-releases-header__title");
 
 $searchTableBtnMob.on("click", function() {
     $searchTableInput.css("width", "100%");
@@ -58,26 +44,31 @@ $searchTableBtnMob.on("click", function() {
     $searchTable.css("grid-column", "1/3");
 });
 
-$searchTable.on("focusout", function() {
-    console.log("lost focus");
-
-    if ((($searchTableInput.val() === null) || ($searchTableInput.val() === "") || ($searchTableInput.val() === undefined))) {
-        $searchTableInput.css("width", "34px");
-        $searchTableClear.css("display", "none");
-        $title.css("display", "block");
-        $searchTable.css("grid-column", "2/3");
-        $searchTableBtnMob.css("right", "0");
-
-    }
-
-});
-
-
+//крестик очистки поля поиска
 $searchTableClear.on("click", function() {
     $searchTableInput.val('');
     $searchTableInput.trigger("focus");
-
 });
+
+$searchTable.on("focusout", function() {
+    if (!(($searchTableInput.val() === null) || ($searchTableInput.val() === "") || ($searchTableInput.val() === undefined))) {
+        $searchTableInput.css("width", "100%");
+    } else {
+        if ($(window).width() <= 575.9) {
+            $searchTableInput.css("width", "34px");
+            $searchTableClear.css("display", "none");
+            $title.css("display", "block");
+            $searchTable.css("grid-column", "2/3");
+            $searchTableBtnMob.css("left", "");
+        } else if ($(window).width() <= 991.9) {
+            $searchTableInput.css("width", "88px");
+        } else {
+            $searchTableInput.css("width", "244px");
+        }
+        $searchTableInput.css("background-color", "white");
+    }
+});
+
 
 
 //переключение между табами в "Все релизы"
