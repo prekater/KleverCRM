24e3f3b02a327bd2aa1576676be41d9b6011be0e
prekater <pepekatya@gmail.com>
2021-03-03 22:36:26 +0300

    adding, all, info contract => common

--- src/track-contract-info/scripts/index.js
+++ src/track-contract-info/scripts/index.js
@@ -1,50 +1,7 @@
-//подтверждение удаления контракта 
-$(function() {
-
-    popupDeleteOverlay = $('.popup-delete')
-    popupConfirm = $(".popup-delete-confirm")
-    $('#contract-delete_btn').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    $('#contract-delete_btn-mobile').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    popupDeleteOverlay.click(function(event) {
-        e = event || window.event
-        if (e.target === this) {
-            popupDeleteOverlay.css('display', 'none')
-            $('body').css('overflow', 'auto')
-        }
-    })
-
-    $('.popup-delete__close').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_cancel').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_confirm').on("click", function() {
-        console.log("Наташ, мы всё удалили");
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-
-    });
-
-});
-
-
 //меню контракта в мобильном
 $(function() {
     if ($(window).width() <= 575.9) {
-        $('.cover-and-name__ellipses').on("click", function() {
+        $('#ellipses_contract').on("click", function() {
             $('.popup-contract').css('display', 'block')
             $('body').css('overflow', 'hidden')
             $(".popup-contract-menu").css('animation', 'translating 0.3s linear 1')
