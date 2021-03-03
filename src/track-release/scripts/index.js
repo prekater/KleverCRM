8da5332c575d03bf73f2ef6267346315582a2fbd
prekater <pepekatya@gmail.com>
2021-03-03 12:06:22 +0300

    fix release info

--- src/track-release/scripts/index.js
+++ src/track-release/scripts/index.js
@@ -36,7 +36,7 @@ $tabReleaseStat.on("click", function() {
 //меню релиза в мобильном
 $(function() {
     if ($(window).width() <= 575.9) {
-        $('.cover-and-name__ellipses').on("click", function() {
+        $('#ellipses_release').on("click", function() {
             $('.popup-release').css('display', 'block')
             $('body').css('overflow', 'hidden')
             $(".popup-release-menu").css('animation', 'translating 0.3s linear 1')
@@ -55,7 +55,7 @@ $(function() {
             $('body').css('overflow', 'auto')
         });
 
-        $('#release-delete_btn-mobile').on("click", function() {
+        $('#item-delete_btn-mobile').on("click", function() {
             $(".popup-release").hide()
             $(".popup-delete").css('display', 'flex')
             $(".popup-delete-confirm").css('animation', 'translating 0.3s linear 1')
@@ -66,45 +66,4 @@ $(function() {
             $('body').css('overflow', 'auto')
         });
     }
-});
-
-//подтверждение удаления релиза
-$(function() {
-    popupDeleteOverlay = $('.popup-delete')
-    popupConfirm = $(".popup-delete-confirm")
-    $('#release-delete_btn').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    $('#release-delete_btn-mobile').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    popupDeleteOverlay.click(function(event) {
-        e = event || window.event
-        if (e.target === this) {
-            popupDeleteOverlay.css('display', 'none')
-            $('body').css('overflow', 'auto')
-        }
-    })
-
-    $('.popup-delete__close').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_cancel').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_confirm').on("click", function() {
-        console.log("Наташ, мы всё удалили");
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-
-    });
-
 });
