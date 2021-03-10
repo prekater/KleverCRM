659dd5ee036396c4af8420c71b7f90dd764be478
prekater <pepekatya@gmail.com>
2021-03-10 14:46:16 +0300

    all platforms, platform => common styles

--- src/track-platform/scripts/index.js
+++ src/track-platform/scripts/index.js
@@ -1,7 +1,7 @@
 //меню платформы в мобильном
 $(function() {
     if ($(window).width() <= 575.9) {
-        $('.cover-and-name__ellipses').on("click", function() {
+        $('#ellipses_platform').on("click", function() {
             $('.popup-platform').css('display', 'block')
             $('body').css('overflow', 'hidden')
             $(".popup-platform-menu").css('animation', 'translating 0.3s linear 1')
@@ -31,45 +31,4 @@ $(function() {
             $('body').css('overflow', 'auto')
         });
     }
-});
-
-//подтверждение удаления платформы
-$(function() {
-    popupDeleteOverlay = $('.popup-delete')
-    popupConfirm = $(".popup-delete-confirm")
-    $('#platform-delete_btn').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    $('#platform-delete_btn-mobile').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    popupDeleteOverlay.click(function(event) {
-        e = event || window.event
-        if (e.target === this) {
-            popupDeleteOverlay.css('display', 'none')
-            $('body').css('overflow', 'auto')
-        }
-    })
-
-    $('.popup-delete__close').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_cancel').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_confirm').on("click", function() {
-        console.log("Наташ, мы всё удалили");
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-
-    });
-
 });
