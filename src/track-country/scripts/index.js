8b047acf573bfe89c7a20839de0e1c4222b795cf
prekater <pepekatya@gmail.com>
2021-03-10 14:36:07 +0300

    country => common styles

--- src/track-country/scripts/index.js
+++ src/track-country/scripts/index.js
@@ -1,7 +1,7 @@
 //меню страны в мобильном
 $(function() {
     if ($(window).width() <= 575.9) {
-        $('.cover-and-name__ellipses').on("click", function() {
+        $('#ellipses_country').on("click", function() {
             $('.popup-country').css('display', 'block')
             $('body').css('overflow', 'hidden')
             $(".popup-country-menu").css('animation', 'translating 0.3s linear 1')
@@ -31,45 +31,4 @@ $(function() {
             $('body').css('overflow', 'auto')
         });
     }
-});
-
-//подтверждение удаления страны
-$(function() {
-    popupDeleteOverlay = $('.popup-delete')
-    popupConfirm = $(".popup-delete-confirm")
-    $('#country-delete_btn').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    $('#country-delete_btn-mobile').on("click", function() {
-        popupDeleteOverlay.css('display', 'flex')
-        $('body').css('overflow', 'hidden')
-        popupConfirm.css('animation', 'translating 0.3s linear 1')
-    });
-    popupDeleteOverlay.click(function(event) {
-        e = event || window.event
-        if (e.target === this) {
-            popupDeleteOverlay.css('display', 'none')
-            $('body').css('overflow', 'auto')
-        }
-    })
-
-    $('.popup-delete__close').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_cancel').on("click", function() {
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-    });
-
-    $('.popup-delete__button_confirm').on("click", function() {
-        console.log("Наташ, мы всё удалили");
-        popupDeleteOverlay.css('display', 'none')
-        $('body').css('overflow', 'auto')
-
-    });
-
 });
